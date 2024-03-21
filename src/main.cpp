d1415da87a403fffc46cd66f8af263fbdd7e6d22
Jia-Baos <18383827268@163.com>
2024-03-21 10:24:04 +0800

    2024-03-21 10:23

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
  * @author Jia-Baos (18383827268@163.com)
- * @brief 通过promise和future实现线程间通信
+ * @brief 原子变量示例
  * @version 0.1
  * @date 2024-03-16
  *
@@ -10,38 +10,30 @@
  */
 
 #include <iostream>
+#include <atomic>
 #include <thread>
-#include <future>
-#include <chrono>
 
-void connect1(std::promise<bool> &flag) {
+std::atomic_flag flag = ATOMIC_FLAG_INIT;
+
+void func(int id) {
+  while (flag.test_and_set(std::memory_order_acquire)) {
+    // 等待其他线程释放锁
+  }
+
+  std::cout << "Thread " << id << " acquired the lock." << std::endl;
+
+  // 模拟业务处理
   std::this_thread::sleep_for(std::chrono::seconds(1));
 
-  flag.set_value(true);
-  // std::cout << "Set the flag to true " << std::endl;
+  flag.clear(std::memory_order_release);  // 释放锁
+  std::cout << "Thread " << id << " released the lock." << std::endl;
 }
 
-void connect2(std::future<bool> &flag) {
-  std::cout << "Status of valid(): " << flag.valid() << ", before get()" << std::endl;
+int main(int argc, char* argv[]) {
+  std::thread t1(func, 1);
+  std::thread t2(func, 2);
 
-  auto flag_tmp = flag.get();
-
-  std::cout << "Status of valid(): " << flag.valid() << ", after get()" << std::endl;
-}
-
-int main(int argc, char *argv[]) {
-  // 声明一个std::promise对象pr1，其保存的值类型为int
-  std::promise<bool> pr1;
-  // 声明一个std::future对象fu1，并通过std::promise的get_future()函数与pr1绑定
-  std::future<bool> fu1 = pr1.get_future();
-
-  // 创建一个线程t1，将函数Thread_Fun1及对象pr1放在线程里面执行
-  std::thread t1(connect1, std::ref(pr1));
-  // 创建一个线程t2，将函数Thread_Fun2及对象fu1放在线程里面执行
-  std::thread t2(connect2, std::ref(fu1));
-
-  // 阻塞至线程结束
-  t1.detach();
+  t1.join();
   t2.join();
 
   return 0;
